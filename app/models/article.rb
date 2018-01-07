ebe8d0eabe1d88834f0f6841f5579c4a032f928f
Moazzam <moazzam.liaqat@gmail.com>
2018-01-07 13:28:06 +0000

    show user info in articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,8 @@ class Article < ActiveRecord::Base
   
   #validates :title, presence: true, length: {minimum: 3, maximum : 50}
   #validates :description, presence: true, length: {mimimum: 3, maximum : 50}
-  
+  belongs_to :user
   validates :title, presence: true, length: {mimimum: 3, maximum: 50}
   validates :description, presence: true, length: {mimimum: 3, maximum: 300}
+  validates :user_id, presence: true
 end
